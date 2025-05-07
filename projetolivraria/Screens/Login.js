67512f36844e32d6606bcd1a5dc705bbde0f1563
Jenifer Vieira <168489623+JeniferSilvaVieira@users.noreply.github.com>
2025-05-07 17:05:10 -0300

    CadastroELogin

--- projetolivraria/Screens/Login.js
+++ projetolivraria/Screens/Login.js
@@ -1,14 +1,14 @@
 import {Text, View, StyleSheet, TextInput, Button} from 'react-native';
 import {useState} from 'react';
-/* import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../controller'; */
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../controller';
 
-export default function Login ({navigation}){
+export default function Vetlogin ({navigation}){
 
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
 
-/*     const VerificarUser = () => {
+    const VerificarUser = () => {
         signInWithEmailAndPassword(auth, email, senha).then(userCredential => {
             console.log('usuario logado', userCredential.user.email);
             navigation.navigate('home');
@@ -16,18 +16,20 @@ export default function Login ({navigation}){
         .catch((error) => {
             console.log('erro ao logar', error.message);
         });
-    } */
+    }
 
     return(
         <View style={styles.container}>
-          <Text style={styles.text1}><b>Super Vet!</b></Text>
+          <Text style={styles.text1}><b>Login - Livraria Tal</b></Text>
             <View style={styles.inputs}>
+                <Text style={styles.text2}><b>Email</b></Text>
                 <TextInput
                 style={styles.input}
                 placeholder="Email"
                 value={email}
                 onChangeText={setEmail}
                 />
+                <Text style={styles.text2}><b>Senha</b></Text>
                 <TextInput
                 style={styles.input}
                 placeholder="Senha"
@@ -36,12 +38,12 @@ export default function Login ({navigation}){
                 secureTextEntry={true}
                 />
             </View>
+            <View style={styles.botoes}>
             <View style={styles.botao}>
             <Button 
             title="Entrar"
             color="#532d0b"
-            /* onPress={VerificarUser} */
-            
+            onPress={VerificarUser}
             />
             </View>
             <View style={styles.botao}>
@@ -50,6 +52,7 @@ export default function Login ({navigation}){
             color="#532d0b"
             onPress={() => navigation.navigate('cadastro')}
             />
+            </View>
             </View>
         </View>
     )
@@ -72,14 +75,23 @@ const styles = StyleSheet.create({
     },
     text1:{
         paddingTop:250,
-        fontSize:20,
+        fontSize:23,
         paddingBottom:40,
-        paddingLeft:145
+        alignSelf:'center',
+    },
+    text2:{
+      paddingLeft:15,
+      fontSize:20
     },
     botao:{
         paddingTop: 20,
         height: 40,
-        width:250,
-        paddingLeft:130,
+        width:120,
+    },
+    botoes:{
+        flexDirection:'row',
+        justifyContent:'space-around',
+        width:'70%',
+        alignSelf:'center'
     }
 })
