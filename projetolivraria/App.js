1317d912d58ffbad7afafa183630b825322c75ae
Jenifer Vieira <168489623+JeniferSilvaVieira@users.noreply.github.com>
2025-05-07 17:06:17 -0300

    app

--- projetolivraria/App.js
+++ projetolivraria/App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './Screens/Login';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Login/>
-    </View>
-  );
+import Cadastro from './Screens/Cadastro';
+import Home
+ from './Screens/Home';
+
+
+// ! TODOS OS DESIGNS E CONFIGURAÇÕES SAO DO OUTRO -- MUDAR!!!
+function BottomTabs(){
+  const Bottom = createBottomTabNavigator();
+
+  return(
+    <Bottom.Navigator
+    screenOptions={{ headerShadowVisible: false, 
+      headerTitleStyle: {fontWeight: 'bold', fontSize: 20}, 
+      tabBarActiveTintColor: '#fff', animation: 'fade' }}> 
+
+      <Bottom.Screen name='Home' component={Home} options={{}}/>
+
+{/*       <Bottom.Screen name='Livros' component={Livros} options={{}}/>
+
+      <Bottom.Screen name='Quadrinhos' component={Quadrinhos} options={{}}/>
+
+      <Bottom.Screen name='Carrinho' component={Carrinho} options={{}}/> */}
+
+    </Bottom.Navigator>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const Stack = createStackNavigator();
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShadowVisible: false, headerStyle: {margin:0}, headerTitleStyle: {fontWeight: 'bold', fontSize: 20} }}>
+
+        <Stack.Screen name='BottomTabs' component={BottomTabs} options={{headerShown: false}} />
+
+        <Stack.Screen name='Cadastro' component={Cadastro} options={{}} />
+{/*         <Stack.Screen name='Login' component={Login} options={{}} />
+
+        <Stack.Screen name='ADMProdutos' component={ADMProdutos} options={{}} />
+
+ */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
