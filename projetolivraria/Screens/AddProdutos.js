3326b66521085c6ecc4907811b2ed75e3cc53c56
rafa <rafaelaaghellere@gmail.com>
2025-05-14 16:50:01 -0300

    add

---
+++ projetolivraria/Screens/AddProdutos.js
@@ -0,0 +1,98 @@
+import {Text, View, StyleSheet, TouchableOpacity, TextInput} from 'react-native'
+import { useState } from 'react';
+import { Picker } from '@react-native-picker/picker';
+import { db } from "../controller";
+import { collection, addDoc } from 'firebase/firestore';
+
+export default function AddProducts(){
+    const [titulo, setTitulo] = useState("");
+    const [autor, setAutor] = useState("");
+    const [sinopse, setSinopse] = useState("");
+    const [editora, setEditora] = useState("");
+    const [categoria, setCategoria] = useState("");
+    const [preco, setPreco] = useState("");
+    const [imagem, setImagem] = useState("");
+
+    const addProduct = async () => {
+        try {
+            await addDoc(collection(db, "produtos"), {
+                titulo: titulo,
+                autor: autor,
+                sinopse: sinopse,
+                editora: editora,
+                categoria: categoria,
+                preco: preco,
+                imagem: imagem,
+                criadoEm: new Date()
+            });
+            alert("Produto adicionado com sucesso!");
+            setTitulo("");
+            setAutor("");
+            setSinopse("");
+            setEditora("");
+            //categoria
+            setPreco("");
+            setImagem("");
+        } catch (error) {
+            console.error("Erro ao adicionar produto: ", error);
+            alert("Erro ao adicionar produto.");
+        }
+    };
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Adicionar Produtos</Text>
+            <View style={{flex:1, alignItems: 'center'}}>
+                <TextInput style={styles.input} placeholder="Título" value={titulo} onChangeText={setTitulo}></TextInput>
+                <TextInput style={styles.input} placeholder="Autor" value={autor} onChangeText={setAutor}></TextInput>
+                <TextInput style={styles.input} placeholder="Sinopse" value={sinopse} onChangeText={setSinopse}></TextInput>
+                <TextInput style={styles.input} placeholder="Editora" value={editora} onChangeText={setEditora}></TextInput>
+                <Picker selectedValue={selectedValue} style={{ height: 50, width: 200 }} onValueChange={(itemValue) => setSelectedValue(itemValue)}>
+                    <Picker.Item label="Livro" value="livro"/>
+                    <Picker.Item label="HQ" value="hq"/>
+                </Picker>
+                <TextInput style={styles.input} placeholder="Preço" value={preco} onChangeText={setPreco}></TextInput>
+                <TextInput style={styles.input} placeholder="Imagem" value={imagem} onChangeText={setImagem}></TextInput>
+                <TouchableOpacity style={styles.botao} onPress={addProduct}>
+                    <Text style={{ color: 'white', fontSize: 26 }}>Enviar</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    ) 
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'rgb(214, 173, 233)',       
+    },
+    input: {
+        fontSize: 23,
+        height: 50,
+        margin: 12,
+        color: 'white',
+        borderColor: 'rgb(117, 64, 192)',
+        borderWidth: 4,
+        padding: 10,
+        borderRadius: 7,
+        backgroundColor:' rgb(168, 128, 223)',
+    },
+    titulo: {
+        marginTop: 30,
+        marginBottom: 20,
+        fontSize: 35,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: 'rgb(46, 6, 83)',
+        textShadowColor: 'rgba(136, 65, 202, 0.49)',
+        textShadowOffset: {width: 2, height: 2},
+    },
+    botao: {
+        width: 200, 
+        height: 60, 
+        backgroundColor: 'rgb(138, 83, 214)', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        borderRadius: 10
+    },
+  });
