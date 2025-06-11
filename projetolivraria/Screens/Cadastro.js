33ac068c38887803de803507bd1d35e6099c2820
Jenifer Vieira <168489623+JeniferSilvaVieira@users.noreply.github.com>
2025-06-11 17:13:20 -0300

    logo

--- projetolivraria/Screens/Cadastro.js
+++ projetolivraria/Screens/Cadastro.js
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet, TextInput, Button, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TextInput, Button, TouchableOpacity, Image } from 'react-native';
 import { useState } from 'react';
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../controller';
@@ -23,6 +23,10 @@ export default function Cadastro({ navigation }) {
 
     return (
         <View style={styles.container}>
+            <Image
+            source={require('../assets/JRI.png')}
+            style={{ width: 120, height: 120,top:100}}
+            />
 
             <Text style={styles.title}>Livraria JRI</Text>
             <Text style={styles.text1}>Cadastro</Text>
