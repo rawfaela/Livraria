6803da3362252b4a580813c2bed2f06c6e2c8149
rafa <rafaelaaghellere@gmail.com>
2025-06-11 16:50:36 -0300

    salvar no banco favs e resenha

--- projetolivraria/Components/FavsProvider.js
+++ projetolivraria/Components/FavsProvider.js
@@ -1,9 +1,62 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { db, auth } from '../controller';
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
 const FavContext = createContext();
 
 export function FavsProvider({children}){
     const [favorito, setFav] = useState([]);
+    const [user, setUser] = useState(null);
+    const [loadingFavs, setLoadingFavs] = useState(true);
+
+    useEffect(()=> {
+        const unsubscribe = onAuthStateChanged(auth, async (user)=>{
+          setUser(user);
+          setLoadingFavs(true);
+    
+          if (user){
+            try {
+              const docRef = doc(db, 'favoritos', user.uid);
+              const docSnap = await getDoc(docRef);
+    
+              if (docSnap.exists()){
+                const data = docSnap.data();
+                setFav(Array.isArray(data.products)? data.products: [])
+              }
+              else{
+                setFav([]);
+              }
+            }
+            catch (error){
+              console.log('Erro no cart', error)
+              setFav([]);
+            }
+          }
+          else{
+            setFav([]);
+          }
+    
+          setLoadingFavs(false);
+        })
+        return () => unsubscribe();
+    }, []);
+
+    useEffect(() => {
+        async function saveFavs(lista) {
+          if (!user || loadingFavs){
+            return;
+          }
+          try {
+            const docRef = doc(db,'favoritos', user.uid);
+            await setDoc(docRef, {products:lista});
+          }
+          catch (error){
+            console.log('erro ao salvar no firebase: ',error);
+          }
+        }
+        saveFavs(favorito)
+        }, [favorito, user, loadingFavs]);
 
     function addToFav(product){
         const alreadyFavorited = favorito.find(item => item.id === product.id);
