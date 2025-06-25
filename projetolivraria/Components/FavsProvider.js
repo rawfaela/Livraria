7dd2169b38a7a09fc7ba676b27fcd78d686cc3bb
rafa <rafaelaaghellere@gmail.com>
2025-06-25 15:21:26 -0300

    Update FavsProvider.js

--- projetolivraria/Components/FavsProvider.js
+++ projetolivraria/Components/FavsProvider.js
@@ -7,80 +7,80 @@ import { onAuthStateChanged } from "firebase/auth";
 const FavContext = createContext();
 
 export function FavsProvider({children}){
-    const [favorito, setFav] = useState([]);
-    const [user, setUser] = useState(null);
-    const [loadingFavs, setLoadingFavs] = useState(true);
+  const [favorito, setFav] = useState([]);
+  const [user, setUser] = useState(null);
+  const [loadingFavs, setLoadingFavs] = useState(true);
 
-    useEffect(()=> {
-        const unsubscribe = onAuthStateChanged(auth, async (user)=>{
-          setUser(user);
-          setLoadingFavs(true);
-    
-          if (user){
-            try {
-              const docRef = doc(db, 'favoritos', user.uid);
-              const docSnap = await getDoc(docRef);
-    
-              if (docSnap.exists()){
-                const data = docSnap.data();
-                setFav(Array.isArray(data.products)? data.products: [])
-              }
-              else{
-                setFav([]);
-              }
-            }
-            catch (error){
-              console.log('Erro nos favoritos', error)
-              setFav([]);
-            }
+  useEffect(()=> {
+    const unsubscribe = onAuthStateChanged(auth, async (user)=>{
+      setUser(user);
+      setLoadingFavs(true);
+
+      if (user){
+        try {
+          const docRef = doc(db, 'favoritos', user.uid);
+          const docSnap = await getDoc(docRef);
+
+          if (docSnap.exists()){
+            const data = docSnap.data();
+            setFav(Array.isArray(data.products)? data.products: [])
           }
           else{
             setFav([]);
           }
-    
-          setLoadingFavs(false);
-        })
-        return () => unsubscribe();
-    }, []);
-
-    useEffect(() => {
-      async function saveFavs(lista) {
-        if (!user || loadingFavs){
-          return;
-        }
-        try {
-          const docRef = doc(db,'favoritos', user.uid);
-          await setDoc(docRef, {products:lista});
         }
         catch (error){
-          console.log('erro ao salvar no firebase: ',error);
+          console.log('Erro nos favoritos', error)
+          setFav([]);
         }
       }
-      saveFavs(favorito)
-      }, [favorito, user, loadingFavs]);
+      else{
+        setFav([]);
+      }
 
-    function addToFav(product){
+      setLoadingFavs(false);
+    })
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    async function saveFavs(lista) {
+      if (!user || loadingFavs){
+        return;
+      }
+      try {
+        const docRef = doc(db,'favoritos', user.uid);
+        await setDoc(docRef, {products:lista});
+      }
+      catch (error){
+        console.log('erro ao salvar no firebase: ',error);
+      }
+    }
+    saveFavs(favorito)
+    }, [favorito, user, loadingFavs]);
+
+  function addToFav(product){
     console.log("Adicionando favorito:", product);
     setFav((anterior) => [...anterior, product]);
-    }
+  }
 
-    function removeFromFav(id){
-      setFav((prevFavoritos) => prevFavoritos.filter((item) => item.id !== id));
-    }
+  function removeFromFav(id){
+    setFav((prevFavoritos) => prevFavoritos.filter((item) => item.id !== id));
+  }
 
-    function isFavorited(id){
-      return favorito.some(item => item.id === id);
-    }
+  function isFavorited(id){
+    return favorito.some(item => item.id === id);
+  }
 
-    return(
-      <FavContext.Provider value={{favorito, addToFav, removeFromFav, isFavorited}}>
-          {children}
-      </FavContext.Provider>
-    )
+  return(
+    <FavContext.Provider value={{favorito, addToFav, removeFromFav, isFavorited}}>
+      {children}
+    </FavContext.Provider>
+  )
 }
 
 export function useFav(){
-    return useContext(FavContext);
+  return useContext(FavContext);
 }
 
 export function FavoriteButton({ bookData, style }) { 
